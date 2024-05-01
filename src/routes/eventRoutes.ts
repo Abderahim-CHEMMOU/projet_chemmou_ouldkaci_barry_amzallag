3c432f1e9e4762a0e82bf3e44c75dabb1035717a
AmineOuldkaci06 <148434447+AmineOuldkaci06@users.noreply.github.com>
2024-05-01 11:35:51 +0200

    delete evente

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -7,9 +7,9 @@ import { eventController } from "../controller/eventController";
 export const setEventRouting = (app) => {
   const endpoint = "events";
 
-  app.get(`/${endpoint}`, eventController.findAll);
-  app.get(`/${endpoint}/:id`, eventController.findById);
-  app.post(`/${endpoint}`, eventController.create);
-  app.patch(`/${endpoint}/:id`, eventController.update);
+  // app.get(`/${endpoint}`, eventController.findAll);
+  // app.get(`/${endpoint}/:id`, eventController.findById);
+  // app.post(`/${endpoint}`, eventController.create);
+  // app.patch(`/${endpoint}/:id`, eventController.update);
   app.delete(`/${endpoint}/:id`, eventController.delete);
 };
