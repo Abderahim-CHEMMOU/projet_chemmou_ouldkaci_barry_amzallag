d3799b3d2c3ab6c50280b3174cbae87703869097
AmineOuldkaci06 <148434447+AmineOuldkaci06@users.noreply.github.com>
2024-05-01 11:38:25 +0200

    delete evente

--- src/controller/eventController.ts
+++ src/controller/eventController.ts
@@ -3,7 +3,24 @@ import { Event, eventJoiSchema } from "../models/event";
 
 class EventController {
  
-
+/**
+   * Suppression d'un événement
+   * @param req
+   * @param res
+   * @param next
+   */
+delete = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const deletedEvent = await Event.findByIdAndDelete(req.params.id);
+      if (!deletedEvent) {
+        return res.status(404).json({ message: "Événement non trouvé" });
+      }
+      res.status(200).json(deletedEvent);
+    } catch (error) {
+      console.error("Erreur lors de la suppression de l'événement :", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  };
   
 }
 
