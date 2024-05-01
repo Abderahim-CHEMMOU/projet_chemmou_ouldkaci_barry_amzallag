1dc2425121d186b7f4c50209ebb549483fb5bff7
Abderahim CHEMMOU <abderahim.chemmou@gmail.com>
2024-05-01 11:45:21 +0200

    create Event

--- src/controller/eventController.ts
+++ src/controller/eventController.ts
@@ -4,7 +4,25 @@ import { Event, eventJoiSchema } from "../models/event";
 class EventController {
  
 
-  
+  /**
+   * Création d'un événement
+   * @param req
+   * @param res
+   * @param next
+   */
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const validationResult = eventJoiSchema.validate(req.body);
+      if (validationResult.error) {
+        return res.status(400).json({ message: validationResult.error.details[0].message });
+      }
+      const event = await Event.create(req.body);
+      res.status(201).json(event);
+    } catch (error) {
+      console.error("Erreur lors de la création de l'événement :", error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  };
 }
 
 export const eventController = Object.freeze(new EventController());
